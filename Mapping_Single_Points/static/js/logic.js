9d5227291d817fa518021afaddeb592b4625383c
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-03-30 18:02:03 -0500

    Added code to plot a singple point

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -17,7 +17,7 @@ let marker = L.marker([34.0522, -118.2437]).addTo(map);
 L.circleMarker([34.0522, -118.2437], {
     color: 'black',
     fillColor: 'ffffa1',    
-    radius: 300
+    radius: 100
  }).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
