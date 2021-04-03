ae42c4e98c568d3d231e11810152364554fd66da
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-04-02 20:43:45 -0500

    Mapping multiple points in leaflet using Mapbox API

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,25 +2,6 @@
 console.log('hank');
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 4);
-
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-//  Add a circle to the map for Los Angeles, California.
-// L.circle([34.0522, -118.2437], {
-//     color: 'black',
-//     fillColor: 'yellow',
-//     fillOpacity: 0.75,
-    
-//     radius: 100
-//  }).addTo(map);
-//Add a circleMarker to the map for LA, Cali
-// L.circleMarker([34.0522, -118.2437], {
-//     color: 'black',
-//     fillColor: 'ffffa1',    
-//     radius: 100
-//  }).addTo(map);
-
-// iterate through cities
 // An array containing each city's location, state, and population.
 let cityData = cities;
   // Loop through the cities array and create one marker for each city.
@@ -41,9 +22,6 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY,
 
 });
-
-
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
