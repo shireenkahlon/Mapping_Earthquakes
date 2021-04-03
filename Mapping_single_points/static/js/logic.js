f473da9c678d110c2b608fc30766386826b3a80b
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-04-02 20:33:30 -0500

    Mapping single points in leaflet using Mapbox API

--- Mapping_single_points/static/js/logic.js
+++ Mapping_single_points/static/js/logic.js
@@ -5,14 +5,7 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //  Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
-//  Add a circle to the map for Los Angeles, California.
-// L.circle([34.0522, -118.2437], {
-//     color: 'black',
-//     fillColor: 'yellow',
-//     fillOpacity: 0.75,
-    
-//     radius: 100
-//  }).addTo(map);
+
 //Add a circleMarker to the map for LA, Cali
 L.circleMarker([34.0522, -118.2437], {
     color: 'black',
