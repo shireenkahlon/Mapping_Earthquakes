a6e5a3be8a328421b774c8e9ba279e7ca0c00189
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-04-02 20:51:51 -0500

    Mapping polygons in leaflet using Mapbox API

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -6,27 +6,23 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     id: 'mapbox/streets-v11',
     accessToken: API_KEY
 });
-
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/satellite-v9',
     accessToken: API_KEY
 });
-
 // Create a base layer that holds both maps.
 let baseMaps = {
     'Streets': streets,
     'Satellite Streets': satelliteStreets
 };
-
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
     center: [43.7, -79.3],
     zoom: 11,
     layers: [satelliteStreets]
 });
-
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
@@ -47,26 +43,6 @@ d3.json(torontoHoods).then(function(data){
     }).addTo(map)
 });
 
-//pointToLayer
-// Grabbing our GeoJSON data.
-// L.geoJSON(sanFranAirport, {
-//         // We turn each feature into a marker on the map.
-//     pointToLayer: function(feature, latlng){
-//         console.log(feature)
-//         return L.marker(latlng)
-//         .bindPopup("<h2>" + feature.properties.city + " , " + feature.properties.country + "</h2><hr><h3>" + feature.properties.name + "</h3>")
-//     }
-// }).addTo(map);
-
-// onEachFeature
-// Grabbing our GeoJSON data.
-// L.geoJSON(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     onEachFeature: function(feature, layer){
-//     console.log(feature)
-//     layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.id + "</h2><hr><h3>" + "Airport Name: as" + feature.properties.name + "</h3>")
-//     }
-// }).addTo(map);
 
 
 
